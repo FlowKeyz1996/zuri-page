f8c0d0715473b2d313744dea27682c02c471ecf5
FlowKeyz1996 <63245360+FlowKeyz1996@users.noreply.github.com>
2021-07-24 05:25:48 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+const menu = document.querySelector("#mobile-menu");
+const menuLinks = document.querySelector(".nav__menu");
+
+
+
+
+menu.addEventListener("click", function(){
+    menu.classList.toggle("is-active");
+    menuLinks.classList.toggle("active");
+});
+
+
+
+
+function realTimeClock(){
+    var rtClock = new Date();
+
+    var hours = rtClock.getHours();
+    var minutes = rtClock.getMinutes();
+    var seconds = rtClock.getSeconds();
+
+
+    var amPm = (hours < 12 ) ? "AM":"PM";
+
+    hours =(hours > 12) ? hours - 12 : hours;
+ 
+    hours = ("0" + hours).slice(-2);
+    minutes = ("0" + minutes).slice(-2);
+    seconds =("0" + seconds).slice(-2);
+
+    document.getElementById("clock").innerHTML =
+    hours + " : " + minutes + " : " + seconds + " " + amPm;
+
+    var t = setTimeout (realTimeClock, 500);
+
+
+
+};
+
+realTimeClock();
